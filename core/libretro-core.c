3d87217b67cb5e2d0da09c501f39362a27b59f2f
Morhir Kimelor <kimelor@gmail.com>
2018-01-20 16:32:13 -0800

    Renames core library form TestCore to TileEngineCore

--- core/libretro-core.c
+++ core/libretro-core.c
@@ -91,7 +91,7 @@ void retro_set_controller_port_device(unsigned port, unsigned device)
 void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(*info));
-   info->library_name     = "TestCore";
+   info->library_name     = "TileEngineCore";
    info->library_version  = "v1";
    info->need_fullpath    = false;
    info->valid_extensions = NULL; // Anything is fine, we don't care.
